91988b49fe652057e9617cadb7790d9a4cc8e0a5
Brando Flores <brandoflores16@gmail.com>
2022-08-05 00:52:02 -0700

    Initial commit

---
+++ SpeakAboutIt/SpeakAboutIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeakAboutIt
+//
+//  Created by Brando Flores on 8/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
