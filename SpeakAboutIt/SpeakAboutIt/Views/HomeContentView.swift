6271b73b70f2687505056fe15d56dc36b42f9671
Brando Flores <brandoflores16@gmail.com>
2022-08-12 13:44:35 -0700

    Add events and explore sections

--- SpeakAboutIt/SpeakAboutIt/Views/HomeContentView.swift
+++ SpeakAboutIt/SpeakAboutIt/Views/HomeContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct HomeContentView: View {
     @State private var showDetails = false
+    var colors: [Color] = [.blue, .green, .red, .orange]
+    var myEventsImages: [Image] = [Image("event-example-3").resizable(), Image("event-example-1").resizable(), Image("event-example-2").resizable()]
     
     init() {
         let appearance = UINavigationBarAppearance()
@@ -23,15 +25,23 @@ struct HomeContentView: View {
         NavigationView {
             ScrollView {
                 VStack {
-                    Button("Show details") {
-                        showDetails.toggle()
+                    VStack {
+                        Text("My Events")
+                            .frame(maxWidth: UIScreen.main.bounds.width, alignment: .leading)
+                            .padding(.leading, 30)
+                            .font(.system(size: 20, weight: .bold))
+                        HStack(alignment: .center, spacing: 10) {
+                            ForEach(0..<myEventsImages.count) { i in
+                                myEventsImages[i]
+                                    .frame(width: 350, height: 200, alignment: .center)
+                                    .cornerRadius(10)
+                            }
+                        }
+                        .modifier(ScrollingHStackModifier(items: myEventsImages.count, itemWidth: 350, itemSpacing: 0))
                     }
-                    
-                    if showDetails {
-                        Text("You should follow me on Twitter: @twostraws")
-                            .font(.largeTitle)
-                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
+                .padding()
                 .frame(maxWidth: .infinity)
                 .navigationBarTitle("", displayMode: .inline)
                 .navigationBarItems(leading:
@@ -51,9 +61,28 @@ struct HomeContentView: View {
                     }
                 }
                 )
-//                ZStack {
-//
-//                }
+                VStack {
+                    HStack {
+                        Image(systemName: "safari")
+                            .imageScale(.large)
+                            .foregroundColor(Color("TabBarHighlight"))
+                        Text("Explore")
+                            .frame(maxWidth: UIScreen.main.bounds.width, alignment: .leading)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                    .frame(maxWidth: UIScreen.main.bounds.width)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0..<myEventsImages.count) { i in
+                                myEventsImages[i]
+                                    .frame(width: 180, height: 100, alignment: .center)
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                .padding()
+                .frame(maxWidth: UIScreen.main.bounds.width)
             }
         }
     }
