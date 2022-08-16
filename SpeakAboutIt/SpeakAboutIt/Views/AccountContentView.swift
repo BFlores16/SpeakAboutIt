9bf3d80bb77b0f9193534afea0fed014cf2331eb
Brando Flores <brandoflores16@gmail.com>
2022-08-15 19:53:26 -0700

    Add more content to home page

---
+++ SpeakAboutIt/SpeakAboutIt/Views/AccountContentView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountContentView.swift
+//  SpeakAboutIt
+//
+//  Created by Brando Flores on 8/12/22.
+//
+
+import SwiftUI
+
+struct AccountContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountContentView()
+    }
+}
--- SpeakAboutIt/SpeakAboutIt/Views/HomeContentView.swift
+++ SpeakAboutIt/SpeakAboutIt/Views/HomeContentView.swift
@@ -26,78 +26,76 @@ struct HomeContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                
-                //if showRegisterBanner {
-                    // Register / signup banner
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(Color("TabBarBackground"))
-                            .cornerRadius(10)
-                            .shadow(radius: 2)
-                        Image(appColorMode == .light ? "Logo" : "Logo-White")
-                            .resizable()
-                            .frame(width: 350, height: 180, alignment: .center)
-                            .opacity(0.03)
-                        VStack {
-                            HStack {
-                                Spacer()
-                                Button(action: {
-                                    withAnimation {
-                                        showRegisterBanner.toggle()
-                                    }
-                                }) {
-                                    ZStack {
-                                        Circle()
-                                            .foregroundColor(Color.black)
-                                            .frame(width: 20, height: 20)
-                                        
-                                        Image(systemName: "x.circle.fill").imageScale(.large).foregroundColor(Color("SpeakAboutItYellow"))
-                                    }
-                                }
-                                .padding([.trailing, .top], 5.0)
-                            }
+        
+                // Register / signup banner
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color("TabBarBackground"))
+                        .cornerRadius(10)
+                        .shadow(radius: 2)
+                    Image(appColorMode == .light ? "Logo" : "Logo-White")
+                        .resizable()
+                        .frame(width: 350, height: 180, alignment: .center)
+                        .opacity(0.03)
+                    VStack {
+                        HStack {
                             Spacer()
-                            Text("Discuss some of the most divisive issues of our time.")
-                                .multilineTextAlignment(.center)
-                                .font(.system(size: 20, weight: .bold))
-                            Text("Sign in or register to attend events")
-                                .multilineTextAlignment(.center)
-                            HStack(spacing: 40) {
-                                Button(action: {
-                                    print("Account register clicked")
-                                }) {
-                                    ZStack {
-                                        Rectangle()
-                                            .frame(width: 100, height: 40, alignment: .center)
-                                            .foregroundColor(Color("SpeakAboutItYellow"))
-                                            .cornerRadius(6)
-                                        Text("Register")
-                                            .font(.system(size: 20, weight: .bold))
-                                            .foregroundColor(Color.black)
-                                    }
+                            Button(action: {
+                                withAnimation {
+                                    showRegisterBanner.toggle()
                                 }
-                                Button(action: {
-                                    print("Account login clicked")
-                                }) {
-                                    ZStack {
-                                        Rectangle()
-                                            .frame(width: 100, height: 40, alignment: .center)
-                                            .foregroundColor(Color.black)
-                                            .cornerRadius(6)
-                                        Text("Log In")
-                                            .font(.system(size: 20, weight: .bold))
-                                            .foregroundColor(Color.white)
-                                    }
+                            }) {
+                                ZStack {
+                                    Circle()
+                                        .foregroundColor(Color.black)
+                                        .frame(width: 20, height: 20)
+                                    
+                                    Image(systemName: "x.circle.fill").imageScale(.large).foregroundColor(Color("SpeakAboutItYellow"))
                                 }
                             }
-                            Spacer()
+                            .padding([.trailing, .top], 5.0)
                         }
-                        .frame(width: screenWidth - 40, height: 200, alignment: .center)
+                        Spacer()
+                        Text("Discuss some of the most divisive issues of our time.")
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 20, weight: .bold))
+                        Text("Sign in or register to attend events")
+                            .multilineTextAlignment(.center)
+                        HStack(spacing: 40) {
+                            Button(action: {
+                                print("Account register clicked")
+                            }) {
+                                ZStack {
+                                    Rectangle()
+                                        .frame(width: 100, height: 40, alignment: .center)
+                                        .foregroundColor(Color("SpeakAboutItYellow"))
+                                        .cornerRadius(6)
+                                    Text("Register")
+                                        .font(.system(size: 20, weight: .bold))
+                                        .foregroundColor(Color.black)
+                                }
+                            }
+                            Button(action: {
+                                print("Account login clicked")
+                            }) {
+                                ZStack {
+                                    Rectangle()
+                                        .frame(width: 100, height: 40, alignment: .center)
+                                        .foregroundColor(Color.black)
+                                        .cornerRadius(6)
+                                    Text("Log In")
+                                        .font(.system(size: 20, weight: .bold))
+                                        .foregroundColor(Color.white)
+                                }
+                            }
+                        }
+                        Spacer()
                     }
                     .frame(width: screenWidth - 40, height: 200, alignment: .center)
-                    .padding()
-                    .HideView(showRegisterBanner, remove: true)
-                //}
+                }
+                .frame(width: screenWidth - 40, height: 200, alignment: .center)
+                .padding()
+                .HideView(showRegisterBanner, remove: true)
                 
                 // My Events scroller
                 VStack {
@@ -161,6 +159,35 @@ struct HomeContentView: View {
                 }
                 .padding()
                 .frame(maxWidth: UIScreen.main.bounds.width)
+                
+                // News scroller
+                VStack {
+                    HStack {
+                        Image(systemName: "newspaper")
+                            .imageScale(.large)
+                            .foregroundColor(Color("TabBarHighlight"))
+                        Text("News")
+                            .frame(maxWidth: UIScreen.main.bounds.width, alignment: .leading)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                    .frame(maxWidth: UIScreen.main.bounds.width)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0..<myEventsImages.count) { i in
+                                myEventsImages[i]
+                                    .frame(width: 180, height: 100, alignment: .center)
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+                .padding()
+                .frame(maxWidth: UIScreen.main.bounds.width)
+                
+                // Keeps scroll content above navigation bar
+                Rectangle()
+                    .frame(width: screenWidth, height: UIScreen.main.bounds.height * 0.1, alignment: .center)
+                    .hidden()
             }
         }
     }
